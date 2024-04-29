f2fe3e7e1e9ddad3258896c0cdf923aff00985b1
Ghifani <ghifaniikhsan114@gmail.com>
2024-04-29 17:02:44 +0700

    Menghubungkan cartSlice ke store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./features/cart/cartSlice";
 
-export default configureStore({})
+export default configureStore({
+    reducer: {
+        cart: cartSlice.reducer,
+    }
+})
